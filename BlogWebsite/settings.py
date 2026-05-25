d6e63670d3c394bb5d154d73c5f740b98985fb1f
ddmcodes <don.denny1234@gmail.com>
2026-05-25 18:59:06 +0530

    Fix Vercel static serving

--- BlogWebsite/settings.py
+++ BlogWebsite/settings.py
@@ -109,18 +109,17 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = '/static/'
+# Static files
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'portfolio' / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
