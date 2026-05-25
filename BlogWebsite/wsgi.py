9cf205cbc435e3d3ef76258905d44de2fab51be5
ddmcodes <don.denny1234@gmail.com>
2026-05-25 16:26:24 +0530

    run migrations at startup

--- BlogWebsite/wsgi.py
+++ BlogWebsite/wsgi.py
@@ -1,7 +1,13 @@
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogWebsite.settings')
+
+django.setup()
+
+# Run migrations at startup
+from django.core.management import call_command
+call_command('migrate', '--run-syncdb', verbosity=0)
 
 application = get_wsgi_application()
