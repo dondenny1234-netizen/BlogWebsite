5dee1a0e9b722407ae00091c7deaee2f14ca6f3e
ddmcodes <don.denny1234@gmail.com>
2026-05-25 17:22:01 +0530

    remove migrate from wsgi

--- BlogWebsite/wsgi.py
+++ BlogWebsite/wsgi.py
@@ -1,12 +1,6 @@
 import os
-import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogWebsite.settings')
-
-django.setup()
-
-from django.core.management import call_command
-call_command('migrate', verbosity=0)
 
 application = get_wsgi_application()
