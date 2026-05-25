6d661b07963a541d1b9422bed11dcb40ca6f089a
ddmcodes <don.denny1234@gmail.com>
2026-05-25 18:02:43 +0530

    hardcode settings module in wsgi

--- BlogWebsite/wsgi.py
+++ BlogWebsite/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogWebsite.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'BlogWebsite.settings'
 
 application = get_wsgi_application()
