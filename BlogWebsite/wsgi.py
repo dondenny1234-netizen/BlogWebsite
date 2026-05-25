88d435a01ff445ffa43f22ea5814488753b762b3
ddmcodes <don.denny1234@gmail.com>
2026-05-25 18:13:54 +0530

    hardcode settings module in wsgi

--- BlogWebsite/wsgi.py
+++ BlogWebsite/wsgi.py
@@ -3,4 +3,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'BlogWebsite.settings'
 
+import sys
+print("SETTINGS MODULE:", os.environ.get('DJANGO_SETTINGS_MODULE'), file=sys.stderr)
+print("PYTHON PATH:", sys.path, file=sys.stderr)
+
 application = get_wsgi_application()
